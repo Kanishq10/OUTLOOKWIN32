8ee7ffb716438a229a58eb855f87b4724f85e75f
Kanishq <79330066+Kanishq10@users.noreply.github.com>
2025-04-07 12:15:22 +0000

    maybe it worls

---
+++ openemail.js
@@ -0,0 +1,18 @@
+const {activeXObject}= require('winax')
+try {
+    const outLook= new activeXObject('Outlook.Apllication');
+    const mapi=outLook.getNamespace('MAPI');
+    const inbox=mapi.GetDefaultFolder(6);
+    const items=inbox.Items;
+    if(items.Count>0){
+        const firstMail=items.Item(1);
+        console.log(firstMail.Subject);
+    }
+    else{
+        console.log("Empty");
+        
+    }
+} catch (error) {
+    console.log("Bye");
+       
+}
